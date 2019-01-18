7e3f748dbba6cbd0ce02bd78bdfc376b47fa0882
PercyJiang <hjiang2@wpi.edu>
2019-01-18 09:41:49 -0500

    Levenshtein

---
+++ nameList.java
@@ -0,0 +1,13 @@
+public class nameList {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Gabe was here");
+        System.out.println("Percy was here too");
+        System.out.println("Harry owo");
+        System.out.println("Clay was here.");
+        System.out.println("Sriiii");
+        System.out.println("Sam was here");
+        System.out.println("rob was here");
+    }
+}
