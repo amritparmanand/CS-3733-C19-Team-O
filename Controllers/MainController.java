0476bbf9afcfab3599ffe55a9b595e1a8b05a717
PercyJiang <hjiang2@wpi.edu>
2019-01-26 11:06:06 -0500

    change scene

--- Controllers/MainController.java
+++ Controllers/MainController.java
@@ -7,10 +7,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class MainController {
-
     /**
      * Change scene
      */
@@ -18,15 +18,23 @@ public class MainController {
     void changeScene(ActionEvent event) throws IOException {
         Stage stage;
         Parent root;
-        if(event.getSource()==register){
+        if(event.getSource()==registerAsManufacturer){
             //get reference to the button's stage
-            stage=(Stage) register.getScene().getWindow();
+            stage=(Stage) registerAsManufacturer.getScene().getWindow();
             //load up OTHER FXML document
-            root = FXMLLoader.load(getClass().getResource("Views/Manufacturer-Homepage-Mockup.fxml"));
+            root = FXMLLoader.load(getClass().getResource("../Views/mRegister.fxml"));
+        }
+        else if(event.getSource()==login){
+            stage=(Stage) login.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("../Views/mHomepage.fxml"));
+        }
+        else if(event.getSource()==register){
+            stage=(Stage) register.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("../Views/mHomepage.fxml"));
         }
         else{
             stage=(Stage) back.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("Views/registerUIMockup.fxml"));
+            root = FXMLLoader.load(getClass().getResource("../Views/LoginPage.fxml"));
         }
         //create a new scene with root and set the stage
         Scene scene = new Scene(root);
@@ -34,18 +42,12 @@ public class MainController {
         stage.show();
     }
 
-    // For LoginTTBUIM
     @FXML
     Button registerAsManufacturer;
-    // End for LoginTTBUIM
-
-    // For registerUIMockup.fxml
     @FXML
     Button register;
-    // End for registerUIMockup.fxml
-
-    // For Manufacturer-Homepage-Mockup.fxml
+    @FXML
+    Button login;
     @FXML
     Button back;
-    // End for Manufacturer-Homepage-Mockup.fxml
 }
