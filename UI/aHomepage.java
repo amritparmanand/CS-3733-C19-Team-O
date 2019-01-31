31d2fc8bbffb78fc9a43f5f218ff1535c95a24c0
PercyJiang <hjiang2@wpi.edu>
2019-01-30 20:51:05 -0500

    empty pop-up

--- UI/aHomepage.java
+++ UI/aHomepage.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class aHomepage {
 
     @FXML private Button back;
     @FXML private Button search;
+    @FXML private VBox getApp;
 
     /**
      * Model
@@ -35,6 +37,10 @@ public class aHomepage {
         else if(event.getSource() == search){
             Parent root = FXMLLoader.load(getClass().getResource("/UI/Views/SearchPage.fxml"));
             sm.changeScene(root, search);
+        }
+        else if(event.getSource() == getApp){
+            Parent root = FXMLLoader.load(getClass().getResource("/UI/Views/aGetApplication.fxml"));
+            sm.changeSceneWithV(root, getApp);
         }
     }
 }
