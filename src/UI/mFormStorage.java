4f9ace69a5a60e6a3981a70065237584d6f31bee
Gabriel Entov <gentov97@gmail.com>
2019-02-03 17:42:08 -0500

    mFormStorage refactored

--- src/UI/mFormStorage.java
+++ src/UI/mFormStorage.java
@@ -1,5 +1,7 @@
 package UI;
 
+import UI.Managers.CacheManager;
+import UI.Managers.DatabaseManager;
 import UI.Managers.SceneManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +13,13 @@ import java.io.IOException;
 
 public class mFormStorage {
 
+    private SceneManager sceneManager;
+    private CacheManager cacheManager;
+    private DatabaseManager databaseManager;
+
     @FXML private Button backToMHome;
     @FXML private Button search;
+    @FXML private Button newForm;
     //@FXML private VBox getApp;
 
     /**
@@ -23,17 +30,28 @@ public class mFormStorage {
     /**
      * Default constructor
      */
-    public mFormStorage() {this.sm = new SceneManager(); }
+
+    public mFormStorage(SceneManager sm, CacheManager cm, DatabaseManager dm) {
+        this.sceneManager = sm;
+        this.cacheManager = cm;
+        this.databaseManager = dm;
+    }
 
     @FXML
-    public void changeScene(MouseEvent event) throws IOException {
-        if(event.getSource() == backToMHome){
-            Parent root = FXMLLoader.load(getClass().getResource("/UI/Views/mHomepage.fxml"));
-            sm.changeScene(root, backToMHome);
-        }
-        else if(event.getSource() == search){
-            Parent root = FXMLLoader.load(getClass().getResource("/UI/Views/SearchPage.fxml"));
-            sm.changeScene(root, search);
-        }
+    public void newForm() throws IOException {
+        //
+        //
+    }
+
+    @FXML
+    public void searchPage() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/Views/SearchPage.fxml"));
+        sceneManager.changeScene(loader, new SearchPage(sceneManager, cacheManager, databaseManager));
+    }
+
+    @FXML
+    public void goToHomePage() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/Views/mHomepage.fxml"));
+        sceneManager.changeScene(loader, new mHomepage(sceneManager, cacheManager, databaseManager));
     }
 }
