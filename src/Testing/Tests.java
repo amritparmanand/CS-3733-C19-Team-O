784df3b4ca97d0b6feb549bcb63512843be0c291
Clay OL <clay.oshiroleavitt@gmail.com>
2019-02-11 17:45:09 -0500

    Rebased Amrit-Rob branch into UI_It2

--- src/Testing/Tests.java
+++ src/Testing/Tests.java
@@ -9,8 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 
 import java.util.Arrays;
