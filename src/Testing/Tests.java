1b48f41a88567b43d03f5c19507d117b0d5fb21e
Clay OL <clay.oshiroleavitt@gmail.com>
2019-02-11 17:58:48 -0500

    refining REGEX

--- src/Testing/Tests.java
+++ src/Testing/Tests.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 
 
