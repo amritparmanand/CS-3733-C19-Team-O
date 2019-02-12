9ef53a1005e0f699474f9fd0172332be94128c82
PercyJiang <hjiang2@wpi.edu>
2019-02-12 18:39:16 -0500

    refactoring code for facade

--- src/Managers/CacheManager.java
+++ src/Managers/CacheManager.java
@@ -5,18 +5,22 @@ import Datatypes.Form;
 import Fuzzy.FuzzyContext;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * @author Sam Silver & Percy
  * @version It 2
  * manages the cache, provides access to cached info
  */
 public class CacheManager {
-//    private Connection dbConn;
+    private Connection dbConn;
     private DatabaseManager dbM;
     private Form form;
     private Account acct;
+    private int formLimit;
 
     public CacheManager(DatabaseManager dbM) {
+        this.dbConn = dbConn;
         this.dbM = dbM;
         this.form = new Form();
     }
@@ -26,14 +30,14 @@ public class CacheManager {
 //        this.form = new Form();
 //    }
 //
-//    public Connection getDbConn() {
-//        return dbConn;
-//    }
-//    public void setDbConn(Connection dbConn) {
-//        this.dbConn = dbConn;
-//    }
 
-    // Getters and setters
+    // Getters and Setters
+    public Connection getDbConn() {
+        return dbConn;
+    }
+    public void setDbConn(Connection dbConn) {
+        this.dbConn = dbConn;
+    }
     public DatabaseManager getDbM() {
         return dbM;
     }
@@ -51,5 +55,24 @@ public class CacheManager {
     }
 
     // Facade stuff
+
+    // The register function
+    public void register(Connection conn){
+        acct.register(dbConn);
+    }
+
+    // Form
+    public void approveForm(Connection conn){
+        form.approve(dbConn);
+    }
+    public void denyForm(Connection conn){
+        form.deny(dbConn);
+    }
+    public void insertApp(Connection connection) throws SQLException {
+        form.addApp(dbConn);
+    }
+    public void insertForm(Connection connection) throws SQLException{
+        form.insert(dbConn);
+    }
 
 }
