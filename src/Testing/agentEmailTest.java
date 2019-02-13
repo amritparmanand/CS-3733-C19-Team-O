64c2c9170bf5c3a8368949a4d36b835690e4b0cd
Clay OL <clay.oshiroleavitt@gmail.com>
2019-02-13 02:21:51 -0500

    testing additional addresses

--- src/Testing/agentEmailTest.java
+++ src/Testing/agentEmailTest.java
@@ -32,7 +32,8 @@ public class agentEmailTest {
     public static Collection<Object[]> emails() {
         return Arrays.asList(new Object[][] {
                 {"clay@TTB.gov", true, true}, {"a@TTB.gov", true, true}, { "Clay123@TTB.gov", true, true}, { "CLAY123@TTB.gov", true, true}, {"clay", false, false}, {"@TTB.gov", false, false}, {"clayTTB.gov", false, false},
-                {"", false, false}, {"clay@gmail.com", false, true}, {"clay123@gmail.com", false, true}, {"CLAY123@gmail.com", false, true}, {"john@budweiser.com", false, true}, {"will@reck.io", false, true}
+                {"", false, false}, {"clay@gmail.com", false, true}, {"clay123@gmail.com", false, true}, {"CLAY123@gmail.com", false, true}, {"john@budweiser.com", false, true}, {"will@reck.io", false, true}, {"clay@a.gov.edu", false, true},
+                {"roo.gov@edu", false, false}
         });
     }
     @Parameterized.Parameter
@@ -47,11 +48,11 @@ public class agentEmailTest {
     // Testing Email Validation Systems
 
     // agent email validation
-//    @Test
-//    public void agentEmailValidation(){
-//        dummyAgent = new aRegister(dummysceneM, dummycacheM);
-//        assertEquals(agentExpected, dummyAgent.validAgentEmail(email));
-//    }
+    @Test
+    public void agentEmailValidation(){
+        dummyAgent = new aRegister(dummysceneM, dummycacheM);
+        assertEquals(agentExpected, dummyAgent.validAgentEmail(email));
+    }
 
     @Test
     public void manufacturerEmailVerification(){
