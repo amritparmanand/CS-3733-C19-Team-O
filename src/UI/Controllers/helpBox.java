706a290aef2318f92ef82786984e18b273a7d41f
sri-kalimani <sriranjani.kalimani@gmail.com>
2019-02-17 14:06:56 -0500

    removed fx-id for help text field

---
+++ src/UI/Controllers/helpBox.java
@@ -0,0 +1,19 @@
+package UI.Controllers;
+
+import Managers.CacheManager;
+import Managers.SceneManager;
+
+public class helpBox {
+    private SceneManager sceneM;
+    private CacheManager cacheM;
+
+    public helpBox(SceneManager sceneM, CacheManager cacheM) {
+        this.sceneM = sceneM;
+        this.cacheM = cacheM;
+    }
+
+//    @FXML
+//    private Text instruction;
+
+
+}
