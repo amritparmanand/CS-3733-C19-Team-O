540db917ad6ce865b7b7dca519f1b43d8090b1df
jeluna <31420664+jeluna@users.noreply.github.com>
2019-02-17 18:36:26 -0500

    helpBox set image function

--- src/UI/Controllers/helpBox.java
+++ src/UI/Controllers/helpBox.java
@@ -2,54 +2,57 @@ package UI.Controllers;
 
 import Managers.CacheManager;
 import Managers.SceneManager;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.fxml.FXML;
 
 public class helpBox {
-    @FXML ImageView helpGif;
-    String prevLocation="login";
-//    private SceneManager sceneM;
-//    private CacheManager cacheM;
-//
-//    public helpBox(SceneManager sceneM, CacheManager cacheM) {
-//        this.sceneM = sceneM;
-//        this.cacheM = cacheM;
-//    }
-    switch(prevLocation){
-        case "login":
-            Image image = new Image(getClass().getResourceAsStream("/HelpGifs/login.png"));
-            helpGif.setFitHeight(726); //726
-            helpGif.setFitWidth(500); //500
-            helpGif.setImage(image);
-            break;
-        case "aAppMulti":
-            Image image = new Image(getClass().getResourceAsStream("/HelpGifs/aApp.png"));
-            helpGif.setFitHeight(726); //726
-            helpGif.setFitWidth(500); //500
-            helpGif.setImage(image);
-            break;
-        case "aAppSingle":
-            Image image = new Image(getClass().getResourceAsStream("/HelpGifs/aApp.png"));
-            helpGif.setFitHeight(726); //726
-            helpGif.setFitWidth(500); //500
-            helpGif.setImage(image);
-            break;
-        case "mAppSingle":
-            Image image = new Image(getClass().getResourceAsStream("/HelpGifs/mAppSingle.png"));
-            helpGif.setFitHeight(726); //726
-            helpGif.setFitWidth(500); //500
-            helpGif.setImage(image);
-            break;
-        case "mAppMulti":
-            Image image = new Image(getClass().getResourceAsStream("/HelpGifs/mApp.png"));
-            helpGif.setFitHeight(726); //726
-            helpGif.setFitWidth(500); //500
-            helpGif.setImage(image);
-            break;
+    @FXML
+    ImageView helpGif;
+    private String prevLocation;
+    private SceneManager sceneM;
+    private CacheManager cacheM;
 
+    public helpBox(SceneManager sceneM, CacheManager cacheM) {
+        this.sceneM = sceneM;
+        this.cacheM = cacheM;
     }
 
-//    @FXML
-//    private Text instruction;
+    public void imageSeter(String prevLocation){
 
+        Image image;
+        switch (prevLocation) {
+            case "login":
+                image = new Image(getClass().getResourceAsStream("/HelpGifs/login.png"));
+                helpGif.setFitHeight(726); //726
+                helpGif.setFitWidth(500); //500
+                helpGif.setImage(image);
+                break;
+            case "aAppMulti":
+                image = new Image(getClass().getResourceAsStream("/HelpGifs/aApp.png"));
+                helpGif.setFitHeight(726); //726
+                helpGif.setFitWidth(500); //500
+                helpGif.setImage(image);
+                break;
+            case "aAppSingle":
+                image = new Image(getClass().getResourceAsStream("/HelpGifs/aApp.png"));
+                helpGif.setFitHeight(726); //726
+                helpGif.setFitWidth(500); //500
+                helpGif.setImage(image);
+                break;
+            case "mAppSingle":
+                image = new Image(getClass().getResourceAsStream("/HelpGifs/mAppSingle.png"));
+                helpGif.setFitHeight(726); //726
+                helpGif.setFitWidth(500); //500
+                helpGif.setImage(image);
+                break;
+            case "mAppMulti":
+                image = new Image(getClass().getResourceAsStream("/HelpGifs/mApp.png"));
+                helpGif.setFitHeight(726); //726
+                helpGif.setFitWidth(500); //500
+                helpGif.setImage(image);
+                break;
 
-
+        }
+    }
 }
