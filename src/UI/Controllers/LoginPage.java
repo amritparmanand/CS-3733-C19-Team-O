dd66b956c2cc737772eb99004994bf15e5d66f04
jeluna <31420664+jeluna@users.noreply.github.com>
2019-02-23 17:20:33 -0500

    Email sending works when loging in, for testing only

--- src/UI/Controllers/LoginPage.java
+++ src/UI/Controllers/LoginPage.java
@@ -21,11 +21,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Statement;
 import java.util.EventListener;
+import java.util.Properties;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class LoginPage implements SerialPortDataListener {
     private SceneManager sceneM;
@@ -204,9 +209,41 @@ public class LoginPage implements SerialPortDataListener {
                 System.out.println("The username and password you entered did not match our records. Please double-check and try again.");
             }
         }
+        // Email testing
+        this.send("ttb.database@gmail.com","OnyxOgopogo","jeluna@wpi.edu","Big Test","It worked?");
     }
 
-    @FXML
+    public static void send(String from,String password,String to,String sub,String msg) {
+        //Get properties object
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        //get Session
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, password);
+                    }
+                });
+        //compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(sub);
+            message.setText(msg);
+            //send message
+            Transport.send(message);
+            System.out.println("message sent successfully");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+        @FXML
     public void register() throws IOException {
         if (m.isSelected()) {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/Views/mRegister.fxml"));
