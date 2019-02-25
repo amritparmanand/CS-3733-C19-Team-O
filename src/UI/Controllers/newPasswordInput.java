7f0e9884b337d89fe2245c362790a7f7744ccb6f
jeluna <31420664+jeluna@users.noreply.github.com>
2019-02-25 03:46:00 -0500

    Moved password reset FXMLs to views folder, removed unused import statements

--- src/UI/Controllers/newPasswordInput.java
+++ src/UI/Controllers/newPasswordInput.java
@@ -1,32 +1,13 @@
 package UI.Controllers;
 
-import Datatypes.Agent;
-import Datatypes.Manufacturer;
 import Datatypes.StageContainingScene;
 import Managers.*;
-
 import Managers.SceneManager;
-import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
 import com.jfoenix.controls.JFXPasswordField;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.*;
-import java.util.EventListener;
-import java.util.Properties;
-
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -53,7 +34,7 @@ public class newPasswordInput extends StageContainingScene {
     }
 
     @FXML
-    public void reset(Connection connection) throws SQLException, java.io.IOException {
+    public void reset(Connection connection) throws SQLException{
         if (password.equals(confirmP)){
             String setData = "UPDATE ? SET PASSWORD = ? WHERE EMAIL = ?";
             PreparedStatement ps = connection.prepareStatement(setData);
@@ -63,8 +44,6 @@ public class newPasswordInput extends StageContainingScene {
             ResultSet rs = ps.executeQuery();
             System.out.println("Password reset! Please login to continue");
             super.getStage().close();
-            //FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/Views/LoginPage.fxml"));
-            //sceneM.changeScene(loader, new LoginPage(sceneM, cacheM));
         }
         else{
             passwordMessage.setTextFill(Color.RED);
