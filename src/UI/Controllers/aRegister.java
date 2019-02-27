a761666eda4cc29fda7067a541d7198f104f88dd
robertrinearson <rrinearsonjr@gmail.com>
2019-02-26 23:05:09 -0500

    alcy update more and bigger boobs and butts 4

--- src/UI/Controllers/aRegister.java
+++ src/UI/Controllers/aRegister.java
@@ -7,6 +7,8 @@ import Managers.*;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
@@ -71,6 +73,73 @@ public class aRegister {
         Alcy alcy = cacheM.getAlcy();
         alcy.summonAlcy(alcyView, alcyLabel);
         alcy.sayRegister();
+
+        username.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue)
+            {
+                if (newPropertyValue)
+                { cacheM.getAlcy().sayHelpUsername();}
+                else
+                {
+                    cacheM.getAlcy().drunk();
+                }
+            }
+        });
+
+        password.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue)
+            {
+                if (newPropertyValue)
+                { cacheM.getAlcy().sayHelpPassword();}
+                else
+                {
+                    cacheM.getAlcy().drunk();
+                }
+            }
+        });
+
+        fullName.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue)
+            {
+                if (newPropertyValue)
+                { cacheM.getAlcy().drunk();}
+                else
+                {
+                    cacheM.getAlcy().sayFullName();
+                }
+            }
+        });
+
+        email.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue)
+            {
+                if (newPropertyValue)
+                { cacheM.getAlcy().sayHelpEmail();}
+                else
+                {
+                    cacheM.getAlcy().drunk();
+                }
+            }
+        });
+
+        phone.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue)
+            {
+                if (newPropertyValue)
+                { cacheM.getAlcy().sayMHelpPhoneNumber();}
+                else
+                {
+                    cacheM.getAlcy().drunk();
+                }
+            }
+        });
+
+
         if(ttbIDFromChip >= 0)
             ttbID.setText(String.valueOf(ttbIDFromChip));
     }
